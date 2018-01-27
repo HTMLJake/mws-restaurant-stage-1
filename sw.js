f73c14c3ea20aca2c2d2dc38b117903d2e040f2a
Jacob Perry <btjake@TheMetalFruit.home>
2018-01-26 20:17:57 -0800

    cache list

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-let index = 7;
+let index = 1;
 const StaticCacheName = "Cache-";
+
+//test
 
 self.addEventListener("fetch", function (event) {
     let e = event.request;
@@ -11,16 +13,28 @@ self.addEventListener("fetch", function (event) {
         })
     );
 
-    event.waitUntil(
+    /* event.waitUntil(
         caches.open(`${StaticCacheName}${index}`).then(function (cache) {
             return cache.add(url.includes('googleapi') ? "" : url);
         })
-    );
+    ); */
 });
+//Array of urls to cache
+let itemsToCache = ['index.html','restaurant.html','/','data/restaurants.json','css/styles.css','js/dbhelper.js','js/main.js', 'js/restaurant_info.js'];
 
 self.addEventListener("install", function (event) {
-    caches.open(`${StaticCacheName}${index}`);
-})
+        caches.open(`${StaticCacheName}${index}`).then(function (cache) {
+            //I am looping through each add adding them individually so if I spell a url wrong in the array I can catch it
+            itemsToCache.forEach(item => {
+                return cache.add(item).catch(function (e) {
+                  console.error(item + " does not exist!");
+                  console.error(e);  
+                });
+            });
+        }).catch(function (err) {
+            console.error(err);
+        });
+});
 
 self.addEventListener('activate', function (event) {
     event.waitUntil(
@@ -29,7 +43,7 @@ self.addEventListener('activate', function (event) {
                 CacheNames.filter(function (cache) {
                     return cache.startsWith(StaticCacheName) && !cache.endsWith(index);
                 }).map(function (cacheName) {
-                    //return caches.delete(cacheName);
+                    return caches.delete(cacheName);
                 })
             );
         })
